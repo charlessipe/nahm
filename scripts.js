78243d34b2bb5909273e2942371ba35b5f0c2370
Charles Sipe <csipe84@gmail.com>
2016-10-09 10:05:16 -0700

    latest data show on top

--- scripts.js
+++ scripts.js
@@ -24,20 +24,34 @@
     rawData.on('value', function(snapshot) { 
     var currentRawData = snapshot.val();
 
-    $(".append-water-table").empty();
+    //$(".append-water-table").empty();
 
+    var reverseSessionData = [];
 
       for (var key in currentRawData) {
         if (currentRawData.hasOwnProperty(key)) {
           
+          reverseSessionData.push(currentRawData[key]);
+          reverseSessionData.reverse();
+
           $(".current-flow").text(currentRawData[key].amount + " liters/second");
 
-          $( ".append-water-table" ).hide().append( "<tr><td><img src='images/shower.png'>" + key + "</td> <td>" + currentRawData[key].time + "</td><td>" + currentRawData[key].amount + "</td> <td>" + currentRawData[key].deviceid + "</td></tr>" ).fadeIn(800);
+          //$( ".append-water-table" ).hide().append( "<tr><td><img src='images/shower.png'>" + key + "</td> <td>" + currentRawData[key].time + "</td><td>" + currentRawData[key].amount + "</td> <td>" + currentRawData[key].deviceid + "</td></tr>" ).fadeIn(800);
 
-          //$( "#1001-amount" ).append( "<td>" + currentRawData[key].amount + "</td>");
-          console.log(currentRawData[key].amount);
+          
 
       };
+
+      console.log(reverseSessionData);
+      
+      $(".append-water-table").empty();
+
+      for (var i = 0; i < reverseSessionData.length; i ++){
+        
+        $( ".append-water-table" ).hide().append( "<tr><td><img src='images/shower.png'>" + i + "</td> <td>" + reverseSessionData[i].time + "</td><td>" + reverseSessionData[i].amount + "</td> <td>" + reverseSessionData[i].deviceid + "</td></tr>" ).fadeIn(800);
+      
+      }
+
 
     };
 

65aedd021cad5b6de03c68409d03ee51379cd8fe
Charles Sipe <csipe84@gmail.com>
2016-10-09 09:23:49 -0700

    Display test data on value change

--- scripts.js
+++ scripts.js
@@ -23,126 +23,33 @@
   var rawData = firebase.database().ref('/test/');
     rawData.on('value', function(snapshot) { 
     var currentRawData = snapshot.val();
-    //console.log(currentRawData); 
 
-    var rawDataArray = [];
+    $(".append-water-table").empty();
 
-    for (var key in currentRawData) {
+
+      for (var key in currentRawData) {
         if (currentRawData.hasOwnProperty(key)) {
-          rawDataArray.push( currentRawData[key].amount);
+          
           $(".current-flow").text(currentRawData[key].amount + " liters/second");
 
-          $( ".append-water-table" ).append( "<tr><td><img src='images/shower.png'>" + key + "</td> <td>" + currentRawData[key].time + "</td><td>" + currentRawData[key].amount + "</td> <td>" + currentRawData[key].deviceid + "</td></tr>" );
+          $( ".append-water-table" ).hide().append( "<tr><td><img src='images/shower.png'>" + key + "</td> <td>" + currentRawData[key].time + "</td><td>" + currentRawData[key].amount + "</td> <td>" + currentRawData[key].deviceid + "</td></tr>" ).fadeIn(800);
 
           //$( "#1001-amount" ).append( "<td>" + currentRawData[key].amount + "</td>");
           console.log(currentRawData[key].amount);
-          /*$( "#1001-device" ).text( deviceData[0]);
-          $( "#1001-time" ).text( timeData[0] );
-          $( "#1001-date" ).text( timeData[0] );
 
-          $( "#1002-amount" ).text( amountData[1] );
-          $( "#1002-device" ).text( deviceData[1]);
-          $( "#1002-time" ).text( timeData[1] );
-          $( "#1002-date" ).text( timeData[1] ); */
-        };
       };
 
-    console.log(rawDataArray);
+    };
+
+
+    /*firebase.database().ref('/test/').once('value').then(function(snapshot) {
+    var oldSessions = snapshot.val();
+    // ...
+    });*/
 
 
   });
 
   
 
-  var waterFlowData0 = firebase.database().ref('/sessions/').once('value').then(function(snapshot) {
-
-    var waterFlowDataValue0 = snapshot.val();
  
-    console.log(waterFlowDataValue0);
-    
-    var amountData = [];
-    var timeData = [];
-    var deviceData = [];
-
-      for (var key in waterFlowDataValue0) {
-        if (waterFlowDataValue0.hasOwnProperty(key)) {
-          amountData.push( waterFlowDataValue0[key].amount );
-          timeData.push( waterFlowDataValue0[key].startTime );
-          deviceData.push( waterFlowDataValue0[key].deviceId );
-        };
-      };
-
-    console.log(amountData);
-    console.log(deviceData);
-    console.log(timeData);
-
-    /*
-    var allWaterFlowData = waterFlowDataValue0[300].amount;
-    var allWaterFlowData2 = waterFlowDataValue0[300].deviceId;
-    var allWaterFlowData3 = waterFlowDataValue0[300].startTime;
-
-    var allWaterFlowData4 = waterFlowDataValue0[301].amount;
-    var allWaterFlowData5 = waterFlowDataValue0[301].deviceId;
-    var allWaterFlowData6 = waterFlowDataValue0[301].startTime;
-    */
-
-    $( "#1001-amount" ).text( amountData[0]);
-    $( "#1001-device" ).text( deviceData[0]);
-    $( "#1001-time" ).text( timeData[0] );
-    $( "#1001-date" ).text( timeData[0] );
-
-    $( "#1002-amount" ).text( amountData[1] );
-    $( "#1002-device" ).text( deviceData[1]);
-    $( "#1002-time" ).text( timeData[1] );
-    $( "#1002-date" ).text( timeData[1] );   
-
-    /*$(".water-flow-table tr").each(function () {
-      $('td').each(function () {
-     })
-    })
-    */
-  
-  });
-
-
-
-
-
-  var waterFlowData = firebase.database().ref('/sessions/300').once('value').then(function(snapshot) {
-
-    var waterFlowDataValue = snapshot.val().amount;
-    // ...
-    console.log(waterFlowDataValue);
-
-    
-    $( "#1001-amount" ).text( waterFlowDataValue + " liters");
-
-  });
-
-  var waterFlowData2 = firebase.database().ref('/sessions/300').once('value').then(function(snapshot) {
-    var waterFlowDataValue2 = snapshot.val().deviceId;
-    // ...
-    console.log(waterFlowDataValue2);
-
-    
-    $( "#1001-device" ).text( waterFlowDataValue2 );
-
-  });
-
-  var waterFlowData3 = firebase.database().ref('/sessions/300').once('value').then(function(snapshot) {
-    var waterFlowDataValue3 = snapshot.val().startTime;
-    // ...
-    console.log(waterFlowDataValue3);
-
-    
-    $( "#1001-time" ).text( waterFlowDataValue3 );
-    $( "#1001-date" ).text( waterFlowDataValue3 );
-
-  });
-
-
-
-  
-
-
-
